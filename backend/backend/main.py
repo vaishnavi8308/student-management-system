122a81602ebc7e5ed970f1d7352652a9a5667e93
Vaishnavi <vaishnaviprathod20@gmail.com>
2026-09-11 15:20:32 +0530

    Connect frontend to Render backend

--- backend/backend/main.py
+++ backend/backend/main.py
@@ -9,9 +9,7 @@ load_dotenv()
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://student-management-system-two-rose.vercel.app"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
