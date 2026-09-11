a5d9766b5343756f9474c01a720ee7b50e826a31
Vaishnavi <vaishnaviprathod20@gmail.com>
2026-09-11 15:07:00 +0530

    Fix backend API URL

--- backend/backend/main.py
+++ backend/backend/main.py
@@ -9,7 +9,9 @@ load_dotenv()
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://student-management-system-two-rose.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
